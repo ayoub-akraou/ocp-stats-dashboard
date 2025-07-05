00af40d77561e8ced7812822fe81e0278f4e5bdb
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-07-05 11:28:48 +0100

    (feat) calculer le taux de rupture (API)

--- backend/app/controllers/ExcelController.js
+++ backend/app/controllers/ExcelController.js
@@ -1,5 +1,24 @@
 const ExcelModel = require("../utils/Excel.js");
 
-class ExcelController {}
+class ExcelController {
+	static getStats(req, res) {
+		try {
+			const data = ExcelModel.lireDonnees();
+			console.log(data);
+
+			// Taux de rupture
+			const totalArticles = data.length;
+			const ruptures = data.filter((ligne) => ligne["SommeDeSTOCK AE"] === 0).length;
+			const tauxRupture = totalArticles ? (ruptures / totalArticles) * 100 : 0;
+
+			res.status(200).json({
+				tauxRupture, ruptures,
+			});
+		} catch (error) {
+			console.error("Erreur dans getStats :", error);
+			res.status(500).json({ message: "Erreur serveur" });
+		}
+	}
+}
 
 module.exports = ExcelController;
