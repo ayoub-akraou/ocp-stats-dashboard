f66de7d4781a4cfbffe1a404c0c3c1c499fc1ba5
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-07-05 11:35:11 +0100

    (test): ajout route temporaire /api/stats (GET)

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,11 @@ app.use(
 app.use(express.json());
 
 const authRoutes = require("./app/routes/auth.routes");
+const ExcelController = require("./app/controllers/ExcelController");
 
 app.use("/api", authRoutes);
+
+app.get("/api/stats", ExcelController.getStats);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Serveur lancé sur le port ${PORT}`));
